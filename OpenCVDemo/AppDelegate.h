6ba29ada984a3757b7fb6983d39922356456f7bf
wanglinwei <wanix1988@gmail.com>
2018-07-02 19:10:19 +0800

    Initial Commit

---
+++ OpenCVDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  OpenCVDemo
+//
+//  Created by wang linwei on 2018/7/2.
+//  Copyright © 2018年 wang linwei. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ OpenCVDemo/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  OpenCVDemo
+//
+//  Created by wang linwei on 2018/7/2.
+//  Copyright © 2018年 wang linwei. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
